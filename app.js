74078e2167609884157b9a6fda39e4c80290da1d
hasindusithmin <hasindusithmin64@gmail.com>
2022-09-22 22:35:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
 const client = require('./db');
 const express = require('express');
+const {faker} = require('@faker-js/faker');
 
 const app = express();
 
@@ -9,6 +10,29 @@ app.use(express.json());
 app.get('/status', (req, res) => {
     res.sendStatus(200);
 });
+
+app.get('/generate/:keyword', (req, res) => {
+
+    switch (req.params.keyword) {
+        case 'firstname':
+            res.send(faker.name.firstName());
+            break;
+        case 'lastname':
+            res.send(faker.name.lastName());
+            break;
+        case 'email':
+            res.send(faker.internet.email());
+            break;
+        case 'city':
+            res.send(faker.address.city());
+            break;
+        case 'country':
+            res.send(faker.address.country());
+            break;
+        default:
+            res.send('Invalid keyword');
+    }
+})
 
 app.get("/",async(req,res)=>{
     try{
